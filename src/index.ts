cd7c5a752c0fd8f84dd1cded9bfe3b580ba44a60
guarU345 <abelmtz34@outlook.com>
2024-03-21 22:05:09 -0600

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.use("", router);
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`server listen on port ${PORT}`);
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -3,9 +3,9 @@ import { TodoController } from "../controllers/TodoController";
 import { Router } from "express";
 const router = Router();
 
-router.get("/api/users/:userId/todos", authenticate, TodoController.getTodos);
-router.post("/api/users/:userId/todos", authenticate, TodoController.createTodo);
-router.put("/api/todos/:id", authenticate, TodoController.updateCompleted);
-router.delete("/api/todos/:id", authenticate, TodoController.deleteTodo);
+router.get("/users/:userId/todos", authenticate, TodoController.getTodos);
+router.post("/users/:userId/todos", authenticate, TodoController.createTodo);
+router.put("/todos/:id", authenticate, TodoController.updateCompleted);
+router.delete("/todos/:id", authenticate, TodoController.deleteTodo);
 
 export const todoRoutes = router;
